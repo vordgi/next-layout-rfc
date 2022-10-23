91dc5f8b97276c91f6a1eb8a49090f792c4f8a3a
Vordgi <sasha2822222@gmail.com>
2022-10-23 21:32:14 +0400

    add link to github

--- src/components/Header/index.server.tsx
+++ src/components/Header/index.server.tsx
@@ -2,25 +2,32 @@ import Link from "next/link";
 
 const Header: React.FC = () => (
     <header>
-        <nav style={{display: 'grid', gridAutoFlow: 'column', justifyContent: 'flex-start', gap: 8}}>
-            <Link href='/'>
-                Home
-            </Link>
-            <Link href='/dashboard/'>
-                Dashboard
-            </Link>
-            <Link href='/profile/'>
-                Profile
-            </Link>
-            <Link href='/about/'>
-                About
-            </Link>
-            <Link href='/product/'>
-                Product
-            </Link>
-            <a href="/parallel/">
-                Parallel
-            </a>
+        <nav style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: 8}}>
+            <div style={{display: 'flex', flexWrap: 'wrap', gap: 8}}>
+                <Link href='/'>
+                    Home
+                </Link>
+                <Link href='/dashboard/'>
+                    Dashboard
+                </Link>
+                <Link href='/profile/'>
+                    Profile
+                </Link>
+                <Link href='/about/'>
+                    About
+                </Link>
+                <Link href='/product/'>
+                    Product
+                </Link>
+                <Link href="/parallel/">
+                    Parallel
+                </Link>
+            </div>
+            <div>
+                <a href="https://github.com/vordgi/next-layout-rfc" target="_blank" rel="noreferrer noopener">
+                    Github
+                </a>
+            </div>
         </nav>
     </header>
 )
